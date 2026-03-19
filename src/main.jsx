7baafd1a177f146819f4c1a0a7a087d4f2cd7f60
kurtise1994-commits <kurtise1994@gmail.com>
2026-03-19 11:06:34 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MinesGame from './MinesGame';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MinesGame />
+  </React.StrictMode>
+);
